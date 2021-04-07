c29c8c925ea715df346c86dbfd34a785d7e055d1
Yuri Mallak <yurimallak@gmail.com>
2021-04-07 14:27:30 -0300

    first commit

---
+++ src/modules/cars/useCases/listCategories/ListCatgoriesController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
+import { ListCategoriesUseCase } from './ListCategoriesUseCase';
+
+class ListCategoriesController {
+    constructor(private ListCategoriesUseCase: ListCategoriesUseCase) {}
+
+    handle(request: Request, response: Response) : Response {
+        const all = this.ListCategoriesUseCase.execute();
+
+        return response.json(all);
+    }
+}
+
+export { ListCategoriesController }

7c2a6859aea231b295b30fcd9ef02466554e0a00
Yuri Mallak <yurimallak@gmail.com>
2021-04-09 17:03:15 -0300

    updating

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,13 +2,13 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { ListCategoriesController } from "./ListCatgoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
-export default(): ListCategoriesController => {
-    const categoriesRepository = new CategoriesRepository();
-    const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
-    const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
 
-    return listCategoriesController;
-}
+const categoriesRepository = null;
+const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
+
+export{ listCategoriesController };
+
 
 
 

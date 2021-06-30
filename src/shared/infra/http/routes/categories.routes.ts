a8cbdd6335327726b76a63a4a7fe809674e237b0
Yuri Mallak <yurimallak@gmail.com>
2021-06-30 16:17:13 -0300

    add admin middleware

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import multer from 'multer';
 import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController";
 import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 
 
 const categoriesRoutes = Router();
@@ -16,7 +18,11 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+    "/", 
+    ensureAuthenticated,
+    ensureAdmin,
+    createCategoryController.handle);
 
 
 categoriesRoutes.get("/", listCategoriesController.handle);
@@ -24,6 +30,8 @@ categoriesRoutes.get("/", listCategoriesController.handle);
 categoriesRoutes.post(
     "/import", 
     upload.single("file"),
+    ensureAuthenticated,
+    ensureAdmin,
     importCategoryController.handle
 );
 
